55a6d44c5f849bfb67eed6d56c4e06ba4d15a6fe
your_username <your_username@donotemail.com>
2022-05-20 02:38:00 +0000

    enable RDS multi-az to match arch diagram

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -23,7 +23,7 @@ resource "aws_db_instance" "db" {
   password                = data.aws_ssm_parameter.dbpassword.value
   availability_zone       = "${var.aws_region}a"
   vpc_security_group_ids  = [aws_security_group.db-sg.id]
-  multi_az                = false
+  multi_az                = true
   db_subnet_group_name    = aws_db_subnet_group.db-subnet-grp.id
   parameter_group_name    = "default.mysql5.7"
   publicly_accessible     = false
